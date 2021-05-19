5f7c46eb6568f374f04aa36e0ea0ea06f2e57df7
Irvig Krehan <irvigkrehan@hotmail.com>
2021-05-19 00:11:01 -0300

    Filter images by imageSize

--- src/services/Scraping.ts
+++ src/services/Scraping.ts
@@ -8,61 +8,83 @@ import News from "./News";
 class Scraping {
 
     async getContent(url) {
-        const { data: html } = await axios.get(url);
-        const $ = Cheerio.load(html);
+        try {
+            const { data: html } = await axios.get(url);
+            const $ = Cheerio.load(html);
 
-        const $element = $('.post-content');
-        const content = $element.find('.content').text().trim();
+            const $element = $('.post-content');
+            const content = $element.find('.content').text().trim();
 
-        return content;
+            return content;
+        } catch (error) {
+            console.log(error);
+
+        }
     }
 
     async getPosts() {
-        const DbCategories = (await Category.index()).data as ICategory[];
-        const validCategories = DbCategories.map((category) => category.path);
+        try {
+            const DbCategories = (await Category.index()).data as ICategory[];
+            const validCategories = DbCategories.map((category) => category.path);
 
-        const DBNews = (await News.index()).data as { currentPage: number; totalPages: number; news: INews[]; };
-        const DbUrls = DBNews.news.map((news) => news.url);
+            const DBNews = (await News.index()).data as { currentPage: number; totalPages: number; news: INews[]; };
+            const DbUrls = DBNews.news.map((news) => news.url);
 
-        const posts = [];
+            const posts = [];
 
-        let [total, current] = [validCategories.length - 1, 0];
-        for (const category of validCategories) {
-            process.stdout.write('\r[Web Scraping] Scraping Categories Posts ' + `${current++}/${total}`);
+            let [total, current] = [validCategories.length, 1];
+            for (const category of validCategories) {
+                const { data: html } = await axios.get('https://jovemnerd.com.br/bunker/categoria' + category);
+                if (!html) continue;
 
-            const { data: html } = await axios.get('https://jovemnerd.com.br/bunker/categoria' + category);
-            if (!html) continue;
+                const $ = Cheerio.load(html);
 
-            const $ = Cheerio.load(html);
+                $('.card-post').each((index, element) => {
+                    const $element = $(element);
+                    const url = $element.find('.image').attr('href');
+                    const title = $element.find('.info .title a').text().trim();
+                    const category = $element.find('.info a').text().trim().split('\n')[0];
+                    const thumbnail = $element.find('a img').attr('src');
 
-            $('.card-post').each((index, element) => {
-                const $element = $(element);
-                const url = $element.find('.image').attr('href');
-                const title = $element.find('.info .title a').text().trim();
-                const category = $element.find('.info a').text().trim().split('\n')[0];
-                const thumbnail = $element.find('a img').attr('src');
+                    const sectionedImageUrl = thumbnail.split('-');
+                    const imageSize = sectionedImageUrl[sectionedImageUrl.length - 1].split('.')[0];
 
-                if (!(DbUrls.includes(url)) && !(posts.map((post) => post.url).includes(url))) {
-                    posts.push({
-                        url,
-                        title,
-                        category,
-                        thumbnail,
-                        content: '',
-                    })
-                }
-            })
+                    if (
+                        !(DbUrls.includes(url)) &&
+                        !(posts.map((post) => post.url).includes(url)) &&
+                        imageSize === '760x428'
+                    ) {
+                        console.log('foi');
+
+                        posts.push({
+                            url,
+                            title,
+                            category,
+                            thumbnail,
+                            content: '',
+                        })
+                    }
+                })
+
+                process.stdout.write('\r[Web Scraping] Scraping Categories Posts ' + `${current++}/${total}`);
+            }
+            console.log(`\n`);
+
+
+            [total, current] = [posts.length, 1];
+            const postsWithContent = []
+            for (const post of posts) {
+                post.content = await this.getContent(post.url);
+                postsWithContent.push(post);
+                process.stdout.write('\r[Web Scraping] Scraping Posts Content ' + `${current++}/${total}`);
+            }
+            console.log(`\n`)
+
+            return postsWithContent;
+
+        } catch (error) {
+            console.log(error);
         }
-        console.log(`\n[Web Scraping] ${current++}/${total} Scrapped Categories Posts`);
-
-        [total, current] = [posts.length - 1, 0];
-        for (const post of posts) {
-            posts[current].content = await this.getContent(post.url)
-            process.stdout.write('\r[Web Scraping] Scraping Posts Content ' + `${current++}/${total}`);
-        }
-        console.log(`\n[Web Scraping] ${current++}/${total} Scrapped Posts Content`)
-
-        return posts;
     }
 }
 
